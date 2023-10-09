5006855eeba4cff1ce904655f9115baf23f9d1b5
Betelgeitze <nikita.belov.w@gmail.com>
2023-10-09 21:34:16 +0200

    First commit

---
+++ dags/user_processing.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+import json
+from pandas import json_normalize
+
+
+
+with DAG('user_processing',
+         start_date=datetime(2023,10,1),
+         schedule_interval='@daily',
+         catchup=False) as dag:
+    
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgres',
+        sql='''
+            CREATE TABLE IF NOT EXISTS users (
+                firstname TEXT NOT NULL,
+                lastname TEXT NOT NULL,
+                country TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                email TEXT NOT NULL
+            );
+        '''
+    )
